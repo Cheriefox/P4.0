c638bd780f356538afd79ed486cdf008295592b1
Cheriefox <164426395+Cheriefox@users.noreply.github.com>
2024-09-12 16:13:17 -0300

    Add files via upload

---
+++ p4_0 (1).sql
@@ -0,0 +1,284 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-09-2024 a las 21:08:00
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `p4.0`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cursos`
+--
+
+CREATE TABLE `cursos` (
+  `ID_cursos` int(15) NOT NULL,
+  `Nombre_Curso` varchar(100) NOT NULL,
+  `Descripcion` varchar(100) NOT NULL,
+  `Contenido` text NOT NULL,
+  `Duracion` varchar(100) NOT NULL,
+  `Modalidad` varchar(100) NOT NULL,
+  `Provedor` varchar(100) NOT NULL,
+  `ID_tecnologia` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `encuestas`
+--
+
+CREATE TABLE `encuestas` (
+  `id_encuesta` int(11) NOT NULL,
+  `creador` varchar(255) NOT NULL,
+  `Pregunta` varchar(100) NOT NULL,
+  `Respuesta` varchar(100) NOT NULL,
+  `duracion_pregunta` int(11) NOT NULL,
+  `Comentarios adicionales` varchar(100) NOT NULL,
+  `id_tecnologia` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `foro`
+--
+
+CREATE TABLE `foro` (
+  `ID_foro` int(11) NOT NULL,
+  `Titulo` varchar(100) NOT NULL,
+  `Descripcion` varchar(100) NOT NULL,
+  `Contenido` text NOT NULL,
+  `ID_tecnologia` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `historia`
+--
+
+CREATE TABLE `historia` (
+  `ID_historia` int(15) NOT NULL,
+  `Titulo` varchar(100) NOT NULL,
+  `Descripcion` varchar(100) NOT NULL,
+  `Contenido` text NOT NULL,
+  `ID_tecnologia` int(50) NOT NULL,
+  `Imagen` varchar(100) NOT NULL,
+  `Orden` int(5) NOT NULL,
+  `Links` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `links`
+--
+
+CREATE TABLE `links` (
+  `ID_links` int(15) NOT NULL,
+  `Enlace` text NOT NULL,
+  `ID_tecnologia` int(10) NOT NULL,
+  `Categoria` varchar(50) NOT NULL,
+  `Descripcion` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `que es`
+--
+
+CREATE TABLE `que es` (
+  `ID_Quees` int(11) NOT NULL,
+  `ID_tecnologia` int(11) NOT NULL,
+  `Titulo` varchar(50) NOT NULL,
+  `Detalle` text NOT NULL,
+  `Imagen` varchar(50) NOT NULL,
+  `Orden` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `secciones`
+--
+
+CREATE TABLE `secciones` (
+  `id_seccion` int(4) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `orden` int(2) NOT NULL,
+  `id_tecnologia` int(11) NOT NULL,
+  `enlace` varchar(40) DEFAULT NULL,
+  `Activo` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tecnologias`
+--
+
+CREATE TABLE `tecnologias` (
+  `ID_tecnologia` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `Descripcion` varchar(100) NOT NULL,
+  `orden` int(100) NOT NULL,
+  `Abreviatura` varchar(4) NOT NULL,
+  `Logo` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `ID_usuario` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `telefono` varchar(15) NOT NULL,
+  `roles` varchar(200) NOT NULL DEFAULT 'Invitado',
+  `gmail` varchar(150) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `Permisos` varchar(10) NOT NULL,
+  `Institucion` varchar(100) NOT NULL,
+  `Rol_institucion` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cursos`
+--
+ALTER TABLE `cursos`
+  ADD PRIMARY KEY (`ID_cursos`);
+
+--
+-- Indices de la tabla `encuestas`
+--
+ALTER TABLE `encuestas`
+  ADD PRIMARY KEY (`id_encuesta`);
+
+--
+-- Indices de la tabla `foro`
+--
+ALTER TABLE `foro`
+  ADD PRIMARY KEY (`ID_foro`);
+
+--
+-- Indices de la tabla `historia`
+--
+ALTER TABLE `historia`
+  ADD PRIMARY KEY (`ID_historia`);
+
+--
+-- Indices de la tabla `links`
+--
+ALTER TABLE `links`
+  ADD PRIMARY KEY (`ID_links`);
+
+--
+-- Indices de la tabla `que es`
+--
+ALTER TABLE `que es`
+  ADD PRIMARY KEY (`ID_Quees`);
+
+--
+-- Indices de la tabla `secciones`
+--
+ALTER TABLE `secciones`
+  ADD PRIMARY KEY (`id_seccion`);
+
+--
+-- Indices de la tabla `tecnologias`
+--
+ALTER TABLE `tecnologias`
+  ADD PRIMARY KEY (`ID_tecnologia`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`ID_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cursos`
+--
+ALTER TABLE `cursos`
+  MODIFY `ID_cursos` int(15) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `encuestas`
+--
+ALTER TABLE `encuestas`
+  MODIFY `id_encuesta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `foro`
+--
+ALTER TABLE `foro`
+  MODIFY `ID_foro` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `historia`
+--
+ALTER TABLE `historia`
+  MODIFY `ID_historia` int(15) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `links`
+--
+ALTER TABLE `links`
+  MODIFY `ID_links` int(15) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `que es`
+--
+ALTER TABLE `que es`
+  MODIFY `ID_Quees` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `secciones`
+--
+ALTER TABLE `secciones`
+  MODIFY `id_seccion` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tecnologias`
+--
+ALTER TABLE `tecnologias`
+  MODIFY `ID_tecnologia` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `ID_usuario` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
